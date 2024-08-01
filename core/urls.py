9a0d653f91966d60720143d0eb2a5e54e5632532
Aarav Poudel <poudelaarav@gmail.com>
2024-08-01 17:58:53 +0545

    added bootstrap layout

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('profile/<int:userid>',profile),
-    path('view_profile/',view_profile),
+    path('view_profile/<str:username>',view_profile),
+    path('view_post/',view_post),
+    path('view_post/<int:id>/',view_post_detail,name="post_detail"),
     path('post/',post_view)
 ]
--- core/views.py
+++ core/views.py
@@ -5,6 +5,32 @@ from .models import *
 from .forms import UserPostForm
 
 
+def view_post_detail(request,id):
+    print(id)
+    post = Post.objects.get(id=id)
+    context ={
+        'post':post
+    }
+    return render(request,'post_detail.html',context)
+
+
+
+def view_post(request):
+    print(request.GET)
+    search_query = request.GET.get('query')
+    if search_query is not None and search_query !="":
+        # yedi search_query pass bhako xa ra khali chaina bhane filter garnu paryo
+        posts = Post.objects.filter(title__icontains=search_query)
+    else:
+        # natra sabai post select garnu paryo
+        posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+    return render(request,'view_post.html',context)
+
+
 def profile(request,userid): 
     user = User.objects.get(id = userid)
     photos = Photos.objects.filter(user = user)
@@ -14,9 +40,21 @@ def profile(request,userid):
     return render(request,'profile.html',context)
 
 
-def view_profile(request):
+def view_profile(request,username):
 
-    return render(request,'profile_page.html')
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return HttpResponse('User with provided username does not exist')
+
+    posts = user.posts.all()  # reverse relationship with related_name
+    print(posts)
+    context = {
+        'user':user,
+        'posts':posts
+    }
+
+    return render(request,'profile_page.html',context)
 
 def post_view(request):
 
@@ -29,13 +67,18 @@ def post_view(request):
     form = UserPostForm()
 
    
+    
     if request.method == 'POST':
         form = UserPostForm(request.POST,request.FILES)
         if form.is_valid():
-            post_obj = form.save(commit=False) # doesn't save in database
-            post_obj.user = request.user
-            post_obj.save()
+            post_object = form.save(commit=False)
+            post_object.user = request.user
+            post_object.save()
+            # post_obj = form.save(commit=False) # doesn't save in database
+            # post_obj.user = request.user
+            # post_obj.save()
             return redirect('/view_profile/')
+    
 
 
     context = {
